59208012f224db00c27b8ff322600cb5e65a8570
sagardevloper004 <sagardevloper004@gmail.com>
2026-03-17 19:41:32 +0530

    list for loop exp

--- str_loop.py
+++ str_loop.py
@@ -1,1 +1,15 @@
-print(1+1)
+arc_address = '''
+Gurudev Nagar Main Rd, beside 
+HPCL Gurudeo Petrol Pump, Chota Tajbag, Nandanvan, Nagpur, Maharashtra 440024
+'''
+    # indent
+
+for ch in arc_address:
+    print(f'ch = {ch}')
+    print('-'*5)
+
+# ch = G
+# ch = u
+# ch = r
+# ....
+# ch = 4
